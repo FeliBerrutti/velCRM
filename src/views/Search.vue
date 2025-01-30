fe1a0b0bc4854f3e2c0d4d9d5ed70f20d34dcc97
Felipe Berrutti <feliberrutti@outlook.com>
2025-01-30 18:44:43 -0300

    change searchView
    delete containers

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,11 +3,37 @@
         <div id="searchViewTitle">
             <b>Buscar</b>
         </div>
-        <SearchContainer></SearchContainer>
+        <div id="searchContainer">
+            <div id="searchContainerLabel">
+                DNI:
+            </div>
+            <input id="searchContainerInput" type="text"/>
+            <button id="searchContainerButton">BUSCAR</button>
+        </div>
         <div id="searchViewResultsLabel">
             <b>Resultados</b>
         </div>
-        <ResultsContainer></ResultsContainer>
+        <div id="resultsContainer">
+            <div class="resultContent"
+                        v-for="(x, index) in resultValueList" :key="index">
+                <div class="resultContentLabel">
+                    {{ x }}
+                </div>
+                <div class="resultContentValue">
+                </div>
+            </div>
+            <!-- <div id="resultContentValueObservations">
+                <div class="observationValue"
+                            v-for="(x, index) in observationValueList" :key="index">
+                    <div class="observationValueLabel">
+                        {{ x }}
+                    </div>
+                    <div class="observationValueContent">
+
+                    </div>
+                </div>
+            </div> -->
+        </div>
     </div>
 </template>
 
@@ -17,6 +43,57 @@
         width: 100%;
     }
 
+    #searchContainer{
+        border: 2px solid black;
+        width: 60%;
+        padding: 1%;
+        display: flex;
+        flex-direction: row;
+    }
+
+    #searchContainerLabel{
+        border: 2px solid red;
+        width: 15%;
+        padding: 0.1%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #searchContainerInput{
+        width: 30%;
+    }
+
+    #resultsContainer{
+        border: 2px solid black;
+        width: 96.5%;
+        padding: 1%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .resultContent{
+        border: 2px solid red;
+        width: 50%;
+        padding: 0.1%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .resultContentLabel{
+        border: 2px solid blue;
+        width: 28%;
+        padding: 0.1%;
+    }
+
+    .resultContentValue{
+        border: 2px solid black;
+        width: 65%;
+        padding: 0.1%;
+    }
+
     @media(max-width: 820px){
         #searchViewContainer{
             min-height: 600px;
@@ -24,10 +101,63 @@
             min-width: 590px;
             max-width: 590px;
         }
+
+        #searchContainer{
+            min-height: 20px;
+            max-height: 20px;
+            min-width: 250px;
+            max-width: 250px;
+        }
+
+        #searchContainerLabel{
+            min-height: 16px;
+            max-height: 16px;
+            min-width: 40px;
+            max-width: 40px;
+            margin-right: 1%;
+        }
+
+        #searchContainerInput{
+            min-height: 15px;
+            max-height: 15px;
+            min-width: 100px;
+            max-width: 100px;
+            margin-right: 2%;
+        }
+
+        #resultsContainer{
+            min-height: 200px;
+            max-height: 200px;
+            min-width: 570px;
+            max-width: 570px;
+        }
+
+        .resultContent{
+            min-height: 25px;
+            max-height: 25px;
+        }
+
+        .resultContentLabel{
+            min-height: 20px;
+            max-height: 20px;
+            min-width: 78px;
+            max-width: 78px;
+            margin-right: 1%;
+        }
+
+        .resultContentValue{
+            min-height: 20px;
+            max-height: 20px;
+            min-width: 190px;
+            max-width: 190px;
+        }
     }
 </style>
 
 <script setup>
-import SearchContainer from '@/components/search/SearchContainer.vue';
-import ResultsContainer from '@/components/search/ResultsContainer.vue';
+    const resultValueList = ['Nombre','Apellido',
+                             'DNI', 'Nacimiento',
+                             'Alta','Baja',
+                             'Plan'
+    ]
 </script>
