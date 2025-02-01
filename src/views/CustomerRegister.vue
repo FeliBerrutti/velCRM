af53e5d796b59ff1d1d7f454f95b6a40e93f8857
Felipe Berrutti <feliberrutti@outlook.com>
2025-02-01 03:25:01 -0300

    build customerRegister
    add observations textarea

--- src/views/CustomerRegister.vue
+++ src/views/CustomerRegister.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="userRegisterViewContainer">
         <div id="userRegisterViewTitle">
-            Registrar Usuario
+            Registrar Cliente
         </div>
         <form id="userRegisterForm" action="">
             <div class="userRegisterFormInputContainer"
@@ -12,6 +12,10 @@
                 </div>
                 <input v-model="customerValues[index]"  class="userRegisterFormInput" type="text">
             </div>
+            <div id="userRegisterFormObservationsTitle">
+                Observaciones
+            </div>
+            <textarea name="" id="userRegisterFormObservationInput" rows="10"></textarea>
             <button @click="add" id="userRegisterFormButton">Registrar</button>
         </form>
     </div>
@@ -31,9 +35,11 @@
     }
 
     #userRegisterForm{
-        border: 2px solid black;
+        border: 2px solid red;
         padding: 1%;
         width: 95%;
+        display: flex;
+        flex-direction: column;
     }
 
     .userRegisterFormInputContainer{
@@ -58,6 +64,10 @@
     #userRegisterFormButton{
         width: 15%;
         padding: 0.5%;
+    }
+
+    #userRegisterFormObservationInput{
+        width: 80%;
     }
 
     @media(max-width: 820px){
@@ -107,6 +117,14 @@
             max-height: 28px;
             min-width: 80px;
             max-width: 80px;
+            margin-top: 2%;
+        }
+
+        #userRegisterFormObservationInput{
+            min-height: 170px;
+            max-height: 170px;
+            min-width: 470px;
+            max-width: 470px;
         }
     }
 </style>
