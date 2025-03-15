4d00f59e18b6f32110000ac96465e3e72f7649ef
Felipe Berrutti <feliberrutti@outlook.com>
2025-03-15 00:30:37 -0300

    Updates

--- src/models/User.js
+++
@@ -1,9 +0,0 @@
-export class User{
-    constructor(id, name, password, rol, created){
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.rol = rol;
-        this.created = created;
-    };
-};
